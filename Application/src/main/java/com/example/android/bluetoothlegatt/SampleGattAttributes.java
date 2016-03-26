e05d3cc74a8a9649cd98ea17661935b21386272f
Mathieu Wang <mathieu.sy.wang@gmail.com>
2016-03-25 20:17:35 -0400

    Remove Heart-Rate and add our UUIDs

--- Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -23,14 +23,39 @@ import java.util.HashMap;
  */
 public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
-    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
-    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    public static String ACC_SERVICE_UUID =     "02366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+    public static String ROLL_CHAR_UUID =       "e23e78a0-cf4a-11e1-8ffc-0002a5d5c51b";
+    public static String PITCH_CHAR_UUID =      "340a1b80-cf4b-11e1-ac36-0002a5d5c51b";
+
+    public static String TEMP_SERVICE_UUID =    "42821a40-e477-11e2-82d0-0002a5d5c51b";
+    public static String TEMP_CHAR_UUID =       "a32e5520-e477-11e2-a9e3-0002a5d5c51b";
+
+//    public static String DOUBLETAP_SERVICE_UUID =    "08366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+//    public static String DOUBLETAP_CHAR_UUID =       "09366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+//
+//    public static String LED_SERVICE_UUID =         "0b366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+//    public static String LED_SPEED_CHAR_UUID =      "0c366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+//    public static String LED_INTENSITY_CHAR_UUID =  "cd20c480-e48b-11e2-840b-0002a5d5c51b";
 
     static {
-        // Sample Services.
-        attributes.put("42821a40-e477-11e2-82d0-0002a5d5c51b", "Environment Sensor Service");
-        // Sample Characteristics.
-        attributes.put("a32e5520-e477-11e2-a9e3-0002a5d5c51b", "Temperature Sensor");
+        // Accelerometer Service
+        attributes.put(ACC_SERVICE_UUID, "Accelerometer Service");
+        attributes.put(ROLL_CHAR_UUID, "Roll Angle");
+        attributes.put(PITCH_CHAR_UUID, "Pitch Angle");
+
+        // Temperature Service
+        attributes.put(TEMP_SERVICE_UUID, "Temperature Service");
+        attributes.put(TEMP_CHAR_UUID, "Temperature");
+
+//        // Doubletap Service
+//        attributes.put(DOUBLETAP_SERVICE_UUID, "Double Tap Service");
+//        attributes.put(DOUBLETAP_CHAR_UUID, "Doble Tap Notification");
+//
+//        // LED Service
+//        attributes.put(LED_SERVICE_UUID, "Accelerometer Service");
+//        attributes.put(LED_SPEED_CHAR_UUID, "LED Speed");
+//        attributes.put(LED_INTENSITY_CHAR_UUID, "LED Intensity");
+
     }
 
     public static String lookup(String uuid, String defaultName) {
